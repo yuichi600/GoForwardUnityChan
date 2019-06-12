4cc5218fb03d750cdc6618cb8fe4036403feeb06
Murata-Optiplex7040 <y.murata@logic-design.jp>
2019-06-12 23:39:15 +0900

    [ Create ] 課題提出

---
+++ Assets/CubeController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+        // キューブの移動速度
+        private float speed = -0.2f;
+
+        // 消滅位置
+        private float deadLine = -10;
+		//課題：音声
+		public AudioClip audioClip;
+		private AudioSource audioSource;
+        
+        // Use this for initialization
+        void Start(){
+        }
+
+        // Update is called once per frame
+        void Update () {
+                // キューブを移動させる
+                transform.Translate (this.speed, 0, 0);
+                
+                // 画面外に出たら破棄する
+                if (transform.position.x < this.deadLine){
+                        Destroy (gameObject);
+                }
+        }
+
+		void OnCollisionEnter2D(Collision2D other) {
+			if(other.gameObject.name!="UnityChan2D"){
+				audioSource = gameObject.GetComponent<AudioSource>();
+    		    audioSource.clip = audioClip;
+	        	audioSource.Play ();
+			}
+		}
+}
